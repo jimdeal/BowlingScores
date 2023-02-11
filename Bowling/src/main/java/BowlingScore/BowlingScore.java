bf17eb7c06b94ecae5cfcc2f8a3308d8e901f008
jimdeal <jimdeal@hotmail.com>
2023-02-11 08:49:27 +0000

    Added comment in the score update / add method - trying to explain approach code has taken.
    One refactor could be to replace code sections that are the same in to their own methods and just call them as needed in the processing tree.
    
    The combinations of incoming frame types and then the break down by previous number of games would not change. But by putting the code in to seperate methods for each identical circumstance - the code in the update / add score method would be significantly reduced.

--- Bowling/src/main/java/BowlingScore/BowlingScore.java
+++ Bowling/src/main/java/BowlingScore/BowlingScore.java
@@ -28,7 +28,7 @@ public class BowlingScore {
         } else if(first + second == 10){
             frameToAdd.frameType = BowlingScore.FrameType.Spare;
         } else {
-
+            //normal type is default
         }
         game.add(frameToAdd);
     }
@@ -42,9 +42,34 @@ public class BowlingScore {
 
         for (int currentGame = 0; currentGame < currentGameSize; currentGame++) {
             BowlingScore.FrameInfo frame1;
-
             frame1 = game.get(currentGame);
 
+            // start by getting frame type of incoming frame
+            // following this : we'll check for preceding game type(s)
+            // at this point we want to go back and see if there are
+            // two preceding games - and if so..
+            //
+            // .. whether we will have to update the scores for those
+            // preceding frame(s) - based on the incoming frame
+            //
+            // TASK NOTE & COMMENT:
+            // This leads to a huge number of combinations of the three
+            // (then two, then one) frames that we need to check and process
+            // to ensure that the current score table is correct.
+            //
+            // This in turn leads to the possibility of refactoring the code
+            // based on patterns of frame processing. That would be the next step.
+            //
+            // HOWEVER: It should also be noted that although the current approach
+            // has led to a fair amount of code repetition : it's also made adding
+            // code for frame combinations relatively straight forward.
+            //
+            // Create new unit test : commit test - then implement / debug so that
+            // new unit test passes & ensuring that ALL previous tests still pass
+            //
+            // This could be a justified approach to development :
+            // complete the task first to attain required unit test coverage
+            // THEN refactor.
             if (frame1.frameType == FrameType.Normal) {
                 if (currentGame - 2 >= 0) {
                     BowlingScore.FrameInfo frame2 = game.get(currentGame - 2);
@@ -81,12 +106,8 @@ public class BowlingScore {
                                 int prevScore = currentScoresByFrame.get(currentGame - 1);
                                 currentFrameFirstScore = frame1.first + frame1.second;
                                 currentScoresByFrame.set(currentGame - 1, currentFrameFirstScore + prevScore);
-
                             }
-
                         }
-
-
                     } else {
                         BowlingScore.FrameInfo frame3 = game.get(currentGame - 1);
                         if (frame3.frameType == FrameType.Spare) {
@@ -108,7 +129,6 @@ public class BowlingScore {
                             currentScoresByFrame.add(currentGame, currentFrameScore);
                         }
                     }
-
                 } else if (currentGame - 1 >= 0) {
                     BowlingScore.FrameInfo frame2 = game.get(currentGame - 1);
                     if (frame2.frameType == FrameType.Spare) {
@@ -122,7 +142,6 @@ public class BowlingScore {
                     }
                     int currentFrameScore = frame1.first + frame1.second;
                     currentScoresByFrame.add(currentGame, currentFrameScore);
-
                 } else {
                     int currentFrameScore = frame1.first + frame1.second;
                     currentScoresByFrame.add(currentGame, currentFrameScore);
@@ -134,9 +153,8 @@ public class BowlingScore {
 
                     if (frame2.frameType == FrameType.Strike) {
                         BowlingScore.FrameInfo frame3 = game.get(currentGame - 1);
-//
-                        if (frame3.frameType == FrameType.Strike) {
 
+                        if (frame3.frameType == FrameType.Strike) {
                             if (frame1.frameType == FrameType.Normal){
 
                             } else if(frame1.frameType == FrameType.Spare){
@@ -149,7 +167,6 @@ public class BowlingScore {
                                 currentScoresByFrame.set(currentGame - 1, currentFrameFirstScore + prevScore);
 
                                 currentScoresByFrame.add(currentGame, currentFrameFirstScore);
-
                             } else {
                                 int prevPrevScore = currentScoresByFrame.get(currentGame - 2);
                                 int currentFrameFirstScore = frame1.first;
@@ -160,10 +177,7 @@ public class BowlingScore {
                                 currentScoresByFrame.set(currentGame - 1, currentFrameFirstScore + prevScore);
 
                                 currentScoresByFrame.add(currentGame, currentFrameFirstScore);
-
                             }
-
-
                         } else if (frame3.frameType == FrameType.Normal) {
                             int currentFrameFirstScore = frame1.first + frame1.second;
                             currentScoresByFrame.add(currentGame, currentFrameFirstScore);
@@ -183,36 +197,25 @@ public class BowlingScore {
                                 int prevScore = currentScoresByFrame.get(currentGame - 1);
                                 currentFrameFirstScore = frame1.first + frame1.second;
                                 currentScoresByFrame.set(currentGame - 1, currentFrameFirstScore + prevScore);
-
                             }
-
                         }
-//
                     } else if (frame2.frameType == FrameType.Normal) {
                         BowlingScore.FrameInfo frame3 = game.get(currentGame - 1);
                         if (frame3.frameType == FrameType.Strike) {
                             int prevScore = currentScoresByFrame.get(currentGame - 1);
                             prevScore += frame1.first + frame1.second;
                             currentScoresByFrame.set(currentGame - 1,  prevScore);
-
-
-
                         }
-//
 
                         int currentFrameScore = frame1.first + frame1.second;
                         currentScoresByFrame.add(currentGame, currentFrameScore);
-
-                    }else { // spare
+                    } else { // spare
                         int prevScore = currentScoresByFrame.get(currentGame - 1);
                         int currentFrameFirstScore = frame1.first;
                         currentScoresByFrame.set(currentGame - 1, currentFrameFirstScore + prevScore);
                         int currentFrameScore = frame1.first + frame1.second;
                         currentScoresByFrame.add(currentGame, currentFrameScore);
-
                     }
-
-
                 } else if (currentGame - 1 >= 0) {
                     BowlingScore.FrameInfo frame2 = game.get(currentGame - 1);
                     if (frame2.frameType == FrameType.Spare) {
@@ -226,12 +229,10 @@ public class BowlingScore {
                     }
                     int currentFrameScore = frame1.first + frame1.second;
                     currentScoresByFrame.add(currentGame, currentFrameScore);
-
                 } else {
                     int currentFrameScore = frame1.first + frame1.second;
                     currentScoresByFrame.add(currentGame, currentFrameScore);
                 }
-
             } else {
                 if (currentGame - 2 >= 0) {
                     BowlingScore.FrameInfo frame3 = game.get(currentGame - 2);
@@ -240,14 +241,8 @@ public class BowlingScore {
                         BowlingScore.FrameInfo frame2 = game.get(currentGame - 1);
                         if (frame2.frameType == FrameType.Normal) {
                             if (frame1.frameType == FrameType.Strike) {
-//                                int prevPrevFrame = currentScoresByFrame.get(currentGame -2);
-//                                int prevScore = currentScoresByFrame.get(currentGame -1);
-//                                currentScoresByFrame.set(currentGame -1, prevPrevFrame + prevScore);
-
-//                                int prevScore = currentScoresByFrame.get(currentGame -1);
                                 int currentFrameFirstScore = frame1.first + frame1.second;
                                 currentScoresByFrame.add(currentGame, currentFrameFirstScore);
-
                             } else {
                                 int prevScore = currentScoresByFrame.get(currentGame - 1);
                                 int currentFrameFirstScore = frame1.first;
@@ -260,7 +255,6 @@ public class BowlingScore {
                                 currentScoresByFrame.set(currentGame - 1, currentFrameFirstScore + prevScore);
                                 currentFrameFirstScore = frame1.first + frame1.second;
                                 currentScoresByFrame.add(currentGame, currentFrameFirstScore);
-
                             } else {
                                 int prevPrevScore = currentScoresByFrame.get(currentGame - 2);
                                 int currentFrameFirstScore = frame1.first;
@@ -269,15 +263,11 @@ public class BowlingScore {
                                 int prevScore = currentScoresByFrame.get(currentGame - 1);
                                 currentFrameFirstScore = frame1.first + frame1.second;
                                 currentScoresByFrame.set(currentGame - 1, currentFrameFirstScore + prevScore);
-
                             }
-
                         } else {
                             int currentFrameFirstScore = frame1.first + frame1.second;
                             currentScoresByFrame.add(currentGame, currentFrameFirstScore);
-
                         }
-
                     } else if ((frame3.frameType == FrameType.Strike)) {
                         BowlingScore.FrameInfo frame2 = game.get(currentGame - 1);
                         if (frame2.frameType == FrameType.Strike) {
@@ -301,7 +291,6 @@ public class BowlingScore {
 
                             currentFrameFirstScore = frame1.first + frame1.second;
                             currentScoresByFrame.add(currentGame, currentFrameFirstScore);
-
                         }
                     } else { // spare
                         int prevScore = currentScoresByFrame.get(currentGame - 1);
@@ -310,7 +299,6 @@ public class BowlingScore {
 
                         currentFrameFirstScore = frame1.first + frame1.second;
                         currentScoresByFrame.add(currentGame, currentFrameFirstScore);
-
                     }
 
                 } else if (currentGame - 1 >= 0) {
@@ -328,18 +316,15 @@ public class BowlingScore {
                         int currentFrameFirstScore = frame1.first;
                         currentScoresByFrame.set(currentGame - 1, currentFrameFirstScore + prevScore);
                         currentScoresByFrame.add(currentGame, currentFrameFirstScore);
-
                     }
-
-
                 } else {
                     int currentFrameScore = frame1.first;
                     currentScoresByFrame.add(currentGame, currentFrameScore);
-
                 }
             }
         }
 
+        // Now we have to look at bonus ball processing - in the tenth frame
         if (game.size() == 10) {
             BowlingScore.FrameInfo lastFrame = game.get(9);
             if (lastFrame.frameType == FrameType.Spare) {
@@ -382,18 +367,18 @@ public class BowlingScore {
                         int prevScore = currentScoresByFrame.get(9);
                         updateprevPrevScore = prevScore + bonusThrows.get(0) + +bonusThrows.get(1);
                         currentScoresByFrame.set(9, updateprevPrevScore);
-
                     }
                 }
             }
         }
 
+        // Now the we have all the score correctly processed - we can total up
+        // the score for the current frame
         for(int totalScore = 0; totalScore<currentScoresByFrame.size();totalScore++){
             currentScore += currentScoresByFrame.get(totalScore);
         }
 
         return currentScore;
-
     }
 
     public void AddBonusThrow(Integer score) {
